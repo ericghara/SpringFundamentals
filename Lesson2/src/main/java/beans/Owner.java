f07e4a169e66ee838ea54d8a767549e384858ed7
ericghara <eric.gharakhanian@gmail.com>
2022-01-25 23:33:04 -0800

    Lesson 2 Commit

---
+++ Lesson2/src/main/java/beans/Owner.java
@@ -0,0 +1,29 @@
+package beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+
+public class Owner {
+
+    @Autowired
+    @Qualifier("Leo")
+    private Cat cat;
+
+    public Cat getCat() {
+        return cat;
+    }
+
+    public void setCat(Cat cat) {
+        this.cat = cat;
+    }
+
+    @Override
+    public String toString() {
+        return "Owner{" +
+                "cat=" + cat +
+                '}';
+    }
+}
